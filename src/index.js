986bb5c1e8b5d8df5f25123d947424897e6c9563
TheWizardJr <nicolas.tonelli@icloud.com>
2023-01-27 18:46:33 +0100

    création dynamique du menu catégories

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 
 const articleContainerElement = document.querySelector(".articles-container");
+const categoriesContainerElement = document.querySelector(".categories");
 
 const createArticles = (articles) => {
   const articlesDOM = articles.map((article) => {
@@ -63,6 +64,32 @@ const createArticles = (articles) => {
   });
 };
 
+const displayMenuCategories = (categoriesArr) => {
+  const liElements = categoriesArr.map((categoryElem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${categoryElem[0]} ( <strong>${categoryElem[1]}</strong> )`;
+    return li;
+  });
+  categoriesContainerElement.innerHTML = "";
+  categoriesContainerElement.append(...liElements);
+};
+
+const createMenuCategories = (articles) => {
+  const categories = articles.reduce((acc, article) => {
+    if (acc[article.category]) {
+      acc[article.category]++;
+    } else {
+      acc[article.category] = 1;
+    }
+    return acc;
+  }, {});
+
+  const categoriesArr = Object.keys(categories).map((category) => {
+    return [category, categories[category]];
+  });
+  displayMenuCategories(categoriesArr);
+};
+
 const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/article");
@@ -71,6 +98,7 @@ const fetchArticle = async () => {
       articles = [articles];
     }
     createArticles(articles);
+    createMenuCategories(articles);
   } catch (e) {
     console.log("e : ", e);
   }
